1f647a21d39b43c768ca2eb99dcd42217f7e8aed
daniel <wolf_daniel24@hotmail.com>
2024-09-15 16:55:51 +0200

    Added

---
+++ 5/run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+#print(__name__)
+#__name__ ist der Modulname, in diesem Fall "run"
+app = Flask(__name__)
+
+#Dekorierer
+#damit wird die Funktion "hello_world" aufgerufen, sobald die Startseite aufgerufen wird
+@app.route("/")
+def hello_world():
+    items = ["Apfel", "Birne", "Banane"]
+    #ruft die start.html Datei aus dem Ordner "templates" auf, mit den zusätzlichen Parametern. In der HTML Datei muss der Parametername gleich sein
+    return render_template("start.html", name = "Max Mustermann", items=items)
+
+#ruft die Function "test_function" auf, wenn "http://127.0.0.1:5000/test" aufgerufen wird
+@app.route("/test")
+def test_function():
+    return render_template("test.html", name="Max Mustermann")
